c1ff7edc760f759f813da1e25c5d7178924e3ee3
Trey Kemp <kemptrey@outlook.com>
2021-05-28 10:44:55 -0600

    starting to pring the phrases

--- app.js
+++ app.js
@@ -4,3 +4,23 @@ const phrases = [
   "I love to code",
   "I love to learn",
 ];
+let i = 0;
+let j = 0;
+let currentPhrase = [];
+
+function loop() {
+  textDisplay.innerHTML = currentPhrase;
+  if (i < phrases.length) {
+    if (j <= phrases[i].length) {
+      currentPhrase.push(phrase[i][j]);
+      j++;
+    }
+
+    if (j == phrases[i].length) {
+      i++;
+    }
+  }
+  setTimeout(loop, 500);
+}
+
+loop();
