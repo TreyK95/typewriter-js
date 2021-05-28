79e3dc9a1a911ae8d685524e9c0f9c0f7dbeae7e
Trey Kemp <kemptrey@outlook.com>
2021-05-28 11:00:35 -0600

    finished project

--- app.js
+++ app.js
@@ -7,20 +7,40 @@ const phrases = [
 let i = 0;
 let j = 0;
 let currentPhrase = [];
+let isDeleting = false;
+let isEnd = false;
 
 function loop() {
+  isEnd = false;
   textDisplay.innerHTML = currentPhrase.join("");
   if (i < phrases.length) {
-    if (j <= phrases[i].length) {
+    if (!isDeleting && j <= phrases[i].length) {
       currentPhrase.push(phrases[i][j]);
       j++;
+      textDisplay.innerHTML = currentPhrase.join("");
     }
 
+    if (isDeleting && j <= phrases[i].length) {
+      currentPhrase.pop(phrases[i][j]);
+      j--;
+    }
     if (j == phrases[i].length) {
+      isEnd = true;
+      isDeleting = true;
+    }
+    if (isDeleting && j === 0) {
+      currentPhrase = [];
+      isDeleting = false;
       i++;
+      if (i == phrases.length) {
+        i = 0;
+      }
     }
   }
-  setTimeout(loop, 500);
+  const spedUP = Math.random() * (80 - 50) + 50;
+  const normalSpeed = Math.random() * (300 - 200) + 200;
+  const time = isEnd ? 2000 : isDeleting ? spedUP : normalSpeed;
+  setTimeout(loop, time);
 }
 
 loop();
